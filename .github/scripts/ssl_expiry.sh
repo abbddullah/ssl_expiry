361967d03c35e2bc940210753d8d7ca2ea5fe168
Mohd Abdullah <93042070+abbddullah@users.noreply.github.com>
2023-08-25 11:58:27 +0530

    Update ssl_expiry.sh

--- .github/scripts/ssl_expiry.sh
+++ .github/scripts/ssl_expiry.sh
@@ -10,7 +10,6 @@ for i in "${DOMAINS[@]}"; do
 
   if [[ $remaining_days -ge 0 && $remaining_days -le 30 ]]; then
     msg="SSL Expiry Alert\n   * Domain: $i\n   * Warning: The SSL certificate for $i will expire in $remaining_days days."
-  curl -X POST -H 'Content-type: application/json' --data '{"text":"$msg"}' "$SLACK_URL"
-
+    curl -X POST -H 'Content-type: application/json' --data '{"text":"test alert"}' https://hooks.slack.com/services/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
   fi
 done

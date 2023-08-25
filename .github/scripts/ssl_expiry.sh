e0e7d6c2e947a224e64054e818f5b7d6f78e3ca2
Mohd Abdullah <93042070+abbddullah@users.noreply.github.com>
2023-08-25 12:27:45 +0530

    Update ssl_expiry.sh

--- .github/scripts/ssl_expiry.sh
+++ .github/scripts/ssl_expiry.sh
@@ -6,7 +6,7 @@ for domain in "${DOMAINS[@]}"; do
   expiry_date=$(echo | openssl s_client -servername "$domain" -connect "$domain":443 2>/dev/null | openssl x509 -noout -dates | awk -F= '/^notAfter/{print $2}')
   expiry_unix=$(date -d "$expiry_date" '+%s')
   current_unix=$(date '+%s')
-  days_until_expiry= 29
+  days_until_expiry= 5
   $(( (expiry_unix - current_unix) / 86400 ))
 
   if [[ $days_until_expiry -ge 0 && $days_until_expiry -le 30 ]]; then

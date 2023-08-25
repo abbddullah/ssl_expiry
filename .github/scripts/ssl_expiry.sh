1f97daecd556011e7bc62de941504c60b02c23c2
Mohd Abdullah <93042070+abbddullah@users.noreply.github.com>
2023-08-25 12:13:12 +0530

    Update ssl_expiry.sh

--- .github/scripts/ssl_expiry.sh
+++ .github/scripts/ssl_expiry.sh
@@ -2,16 +2,15 @@
 SLACK_URL="$1"
 DOMAINS=("www.google.com" "www.github.com" "www.twitter.com")
 
-for i in "${DOMAINS[@]}"; do 
-  date_of_expiry=$(echo | openssl s_client -servername "$i" -connect "$i":443 2>/dev/null | openssl x509 -noout -dates | awk -F= '/^notAfter/{print $2}')
-  converted_expiry_date=$(date -d "$date_of_expiry" '+%s')
-  current_date=$(date '+%s')
-  remaining_days=7
-  #$(( (converted_expiry_date - current_date) / 86400 ))
+for domain in "${DOMAINS[@]}"; do 
+  expiry_date=$(echo | openssl s_client -servername "$domain" -connect "$domain":443 2>/dev/null | openssl x509 -noout -dates | awk -F= '/^notAfter/{print $2}')
+  expiry_unix=$(date -d "$expiry_date" '+%s')
+  current_unix=$(date '+%s')
+  days_until_expiry=$(( (expiry_unix - current_unix) / 86400 ))
 
-  if [[ $remaining_days -ge 0 && $remaining_days -le 30 ]]; then
-    msg="SSL Expiry Alert   \n* Domain: $i\n   * Warning: The SSL certificate for $i will expire in $remaining_days days."
+  if [[ $days_until_expiry -ge 0 && $days_until_expiry -le 30 ]]; then
+    message="SSL Expiry Alert\n* Domain: $domain\n* Warning: The SSL certificate for $domain will expire in $days_until_expiry days."
     payload="{\"text\":\"$message\"}"
-    curl -X POST -H 'Content-type: application/json' --data '{"text":"test alert"}' "$SLACK_URL"
+    curl -X POST -H 'Content-type: application/json' --data "$payload" "$SLACK_URL"
   fi
 done

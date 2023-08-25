8d2df3ab19fe39e6f75a3903160829f8f413f11d
Mohd Abdullah <93042070+abbddullah@users.noreply.github.com>
2023-08-25 12:01:27 +0530

    Update ssl_expiry.sh

--- .github/scripts/ssl_expiry.sh
+++ .github/scripts/ssl_expiry.sh
@@ -6,10 +6,11 @@ for i in "${DOMAINS[@]}"; do
   date_of_expiry=$(echo | openssl s_client -servername "$i" -connect "$i":443 2>/dev/null | openssl x509 -noout -dates | awk -F= '/notAfter/{print $2}')
   converted_expiry_date=$(date -d "$date_of_expiry" '+%s')
   current_date=$(date '+%s')
-  remaining_days=$(( (converted_expiry_date - current_date) / 86400 ))
+  remaining_days=7
+  #$(( (converted_expiry_date - current_date) / 86400 ))
 
   if [[ $remaining_days -ge 0 && $remaining_days -le 30 ]]; then
     msg="SSL Expiry Alert\n   * Domain: $i\n   * Warning: The SSL certificate for $i will expire in $remaining_days days."
-    
+    curl -X POST -H 'Content-type: application/json' --data '{"text":"test alert"}' "$SLACK_URL"
   fi
 done

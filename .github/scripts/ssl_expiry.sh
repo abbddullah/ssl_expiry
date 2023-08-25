db74adb73def93ead680d0986eb781031f0e0a18
Mohd Abdullah <93042070+abbddullah@users.noreply.github.com>
2023-08-25 12:08:32 +0530

    Update ssl_expiry.sh

--- .github/scripts/ssl_expiry.sh
+++ .github/scripts/ssl_expiry.sh
@@ -3,14 +3,15 @@ SLACK_URL="$1"
 DOMAINS=("www.google.com" "www.github.com" "www.twitter.com")
 
 for i in "${DOMAINS[@]}"; do 
-  date_of_expiry=$(echo | openssl s_client -servername "$i" -connect "$i":443 2>/dev/null | openssl x509 -noout -dates | awk -F= '/notAfter/{print $2}')
+  date_of_expiry=$(echo | openssl s_client -servername "$i" -connect "$i":443 2>/dev/null | openssl x509 -noout -dates | awk -F= '/^notAfter/{print $2}')
   converted_expiry_date=$(date -d "$date_of_expiry" '+%s')
   current_date=$(date '+%s')
   remaining_days=7
   #$(( (converted_expiry_date - current_date) / 86400 ))
 
   if [[ $remaining_days -ge 0 && $remaining_days -le 30 ]]; then
-    msg="SSL Expiry Alert\n   * Domain: $i\n   * Warning: The SSL certificate for $i will expire in $remaining_days days."
+    msg="SSL Expiry Alert   \n* Domain: $i\n   * Warning: The SSL certificate for $i will expire in $remaining_days days."
+    payload="{\"text\":\"$message\"}"
     curl -X POST -H 'Content-type: application/json' --data '{"text":"test alert"}' "$SLACK_URL"
   fi
 done

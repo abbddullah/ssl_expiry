c3c9a75fe0aacef7c0da985b6e59daa9b602e433
Mohd Abdullah <93042070+abbddullah@users.noreply.github.com>
2023-08-25 12:24:59 +0530

    Update ssl_expiry.sh

--- .github/scripts/ssl_expiry.sh
+++ .github/scripts/ssl_expiry.sh
@@ -6,7 +6,8 @@ for domain in "${DOMAINS[@]}"; do
   expiry_date=$(echo | openssl s_client -servername "$domain" -connect "$domain":443 2>/dev/null | openssl x509 -noout -dates | awk -F= '/^notAfter/{print $2}')
   expiry_unix=$(date -d "$expiry_date" '+%s')
   current_unix=$(date '+%s')
-  days_until_expiry=$(( (expiry_unix - current_unix) / 86400 ))
+  days_until_expiry= 29
+  $(( (expiry_unix - current_unix) / 86400 ))
 
   if [[ $days_until_expiry -ge 0 && $days_until_expiry -le 30 ]]; then
     message="SSL Expiry Alert\n* Domain: $domain\n* Warning: The SSL certificate for $domain will expire in $days_until_expiry days."

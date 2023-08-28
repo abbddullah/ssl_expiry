0dfca6af9c3ee2c54ab90f47751e5f7e887b0c45
Mohd Abdullah <93042070+abbddullah@users.noreply.github.com>
2023-08-28 13:48:37 +0530

    Update ssl_expiry_test.sh

--- .github/scripts/ssl_expiry_test.sh
+++ .github/scripts/ssl_expiry_test.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 SLACK_URL="$1"
-DOMAINS=("www.google.com" "www.github.com" "www.twitter.com")
+DOMAINS="domain_list.txt"
+readarray -t DOMAINS < "$DOMAINS"
 
 for domain in "${DOMAINS[@]}"; do 
   expiry_date=$(echo | openssl s_client -servername "$domain" -connect "$domain":443 2>/dev/null | openssl x509 -noout -dates | awk -F= '/^notAfter/{print $2}')
   expiry_unix=$(date -d "$expiry_date" '+%s')
   current_unix=$(date '+%s')
-  days_until_expiry= 5
+  days_until_expiry= 21
   #$(( (expiry_unix - current_unix) / 86400 ))
 
   if [[ $days_until_expiry -ge 0 && $days_until_expiry -le 30 ]]; then
